188f71b5cb7f0f6972f246e2b9bdcb07d2ee407e
rjelenius <robert.jelenius@gmail.com>
2022-11-18 11:12:34 +0100

    Added unit test project

---
+++ Hackerservice/HackerService.UnitTests/KryptoTests.cs
@@ -0,0 +1,17 @@
+namespace HackerService.UnitTests
+{
+    public class KryptoTests
+    {
+        [Fact]
+        public void Encrypt_Should_WhenBlaBla()
+        {
+
+        }
+
+        [Fact]
+        public void Decrypt_Should_WhenBlaBla()
+        {
+
+        }
+    }
+}
